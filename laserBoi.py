3847379f2f0da5dcbb4ac485cb3999ab283cfb02
evan1533 <33203039+evan1533@users.noreply.github.com>
2018-01-31 15:49:05 -0500

    Update laserBoi.py

--- laserBoi.py
+++ laserBoi.py
@@ -14,6 +14,9 @@ def loop():
 		print '...led on'
 		GPIO.output(LedPin, GPIO.HIGH)  # led on
 		time.sleep(0.5)
+		print 'led off...'
+		GPIO.output(LedPin, GPIO.LOW) # led off
+		time.sleep(0.5)
 
 def destroy():
 	GPIO.output(LedPin, GPIO.LOW)     # led off
