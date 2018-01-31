6b487d75d18851f3bc9380e6ae5f084098b4bd5f
evan1533 <33203039+evan1533@users.noreply.github.com>
2018-01-31 16:00:12 -0500

    Update ultraLaser.py

--- ultraLaser.py
+++ ultraLaser.py
@@ -1,27 +1,59 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time
-
-LedPin = 11    # pin11
+TRIG = 12
+ECHO = 16
+LedPin = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+ultraClean = False
 
 def setup():
+    	GPIO.setmode(GPIO.BOARD)
+    	GPIO.setup(TRIG, GPIO.OUT)
+    	GPIO.setup(ECHO, GPIO.IN)
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
 	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
-
+def distance():
+	GPIO.output(TRIG, 0)
+	time.sleep(0.000002)
+	GPIO.output(TRIG, 1)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, 0)
+	time1 = 0
+	time2 = 0
+	while GPIO.input(ECHO) == 0:
+		a = 0
+		time1 = time.time()
+	while GPIO.input(ECHO) == 1:
+		a = 1
+		time2 = time.time()
+	during = time2 - time1
+	during = during * 340 / 2 * 100
+	if during >2 and during <4:
+		ultraClean = True
+		GPIO.cleanup()
+	return during
+    
 def loop():
-	while True:
-		print '...led on'
-		GPIO.output(LedPin, GPIO.LOW)  # led on
-		time.sleep(0.5)
-
+    while True:
+        if not ultraClean:
+            dis = distance()
+            print dis, 'cm'
+            print ''
+        time.sleep(0.3)
+        
 def destroy():
-	GPIO.output(LedPin, GPIO.LOW)     # led off
-	GPIO.cleanup()                     # Release resource
+	GPIO.cleanup()
+	GPIO.output(LedPin, GPIO.LOW)   
 
-if __name__ == '__main__':     # Program start from here
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destroy()
+if __name__ == "__main__":
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+
+
