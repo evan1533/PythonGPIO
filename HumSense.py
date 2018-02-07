db740625e4e7defd857b4051a0d38b68a7014944
evan1533 <33203039+evan1533@users.noreply.github.com>
2018-02-07 14:03:51 -0500

    Create HumSense.py

---
+++ HumSense.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+MS = 17
+GPIO.setmode(GPIO.BCM)
+
+def setup():
+	GPIO.setup(MS, GPIO.IN)
+  
+def loop():
+	status = 1
+	while True:
+		tmp = GPIO.input(DO);
+    print tmp
+		
+		time.sleep(0.2)
+
+if __name__ == '__main__':
+	try:
+		setup()
+		loop()
+	except KeyboardInterrupt: 
+		pass	
