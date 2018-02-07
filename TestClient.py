7a798dfb434fae3be451a4a158fa37e8c7c411a0
evan1533 <evan1533@vt.edu>
2018-02-07 14:55:54 -0500

    Servers

--- TestClient.py
+++ TestClient.py
@@ -2,9 +2,9 @@ import socket
 import sys
 
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
+sock = socket.socket();
 server_address = ('172.24.66.145', 10000)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
