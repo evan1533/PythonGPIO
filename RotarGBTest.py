10a5a2661ac68981c800fe6958a45e71e2dda86a
evan1533 <33203039+evan1533@users.noreply.github.com>
2018-04-16 17:35:40 -0400

    Create RotarGBTest.py

---
+++ RotarGBTest.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+RoAPin = 11    # CLK Pin
+RoBPin = 12    # DT Pin
+BtnPin = 13    # Button Pin
+
+globalCounter = 0
+
+flag = 0
+Last_RoB_Status = 0
+Current_RoB_Status = 0
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+	GPIO.setup(RoAPin, GPIO.IN)    # input mode
+	GPIO.setup(RoBPin, GPIO.IN)
+	GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def rotaryDeal():
+	global flag
+	global Last_RoB_Status
+	global Current_RoB_Status
+	global globalCounter
+	Last_RoB_Status = GPIO.input(RoBPin)
+	while(not GPIO.input(RoAPin)):
+		Current_RoB_Status = GPIO.input(RoBPin)
+		flag = 1
+	if flag == 1:
+		flag = 0
+		if (Last_RoB_Status == 0) and (Current_RoB_Status == 1):
+			globalCounter = globalCounter + 1
+		if (Last_RoB_Status == 1) and (Current_RoB_Status == 0):
+			globalCounter = globalCounter - 1
+
+def btnISR(channel):
+	global globalCounter
+	globalCounter = 0
+
+def loop():
+	global globalCounter
+	tmp = 0	# Rotary Temperary
+
+	GPIO.add_event_detect(BtnPin, GPIO.FALLING, callback=btnISR)
+	while True:
+		rotaryDeal()
+		if tmp != globalCounter:
+			print 'globalCounter = %d' % globalCounter
+			tmp = globalCounter
+
+def destroy():
+	GPIO.cleanup()             # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
+colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF]
+R = 11
+G = 12
+B = 18
+
+def setup(Rpin, Gpin, Bpin):
+	global pins
+	global p_R, p_G, p_B
+	pins = {'pin_R': Rpin, 'pin_G': Gpin, 'pin_B': Bpin}
+	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+	for i in pins:
+		GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
+		GPIO.output(pins[i], GPIO.HIGH) # Set pins to high(+3.3V) to off led
+	
+	p_R = GPIO.PWM(pins['pin_R'], 2000)  # set Frequece to 2KHz
+	p_G = GPIO.PWM(pins['pin_G'], 1999)
+	p_B = GPIO.PWM(pins['pin_B'], 5000)
+	
+	p_R.start(100)      # Initial duty Cycle = 0(leds off)
+	p_G.start(100)
+	p_B.start(100)
+
+def map(x, in_min, in_max, out_min, out_max):
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+def off():
+	for i in pins:
+		GPIO.output(pins[i], GPIO.HIGH)    # Turn off all leds
+
+def setColor(col):   # For example : col = 0x112233
+	R_val = (col & 0xff0000) >> 16
+	G_val = (col & 0x00ff00) >> 8
+	B_val = (col & 0x0000ff) >> 0
+
+	R_val = map(R_val, 0, 255, 0, 100)
+	G_val = map(G_val, 0, 255, 0, 100)
+	B_val = map(B_val, 0, 255, 0, 100)
+	
+	p_R.ChangeDutyCycle(100-R_val)     # Change duty cycle
+	p_G.ChangeDutyCycle(100-G_val)
+	p_B.ChangeDutyCycle(100-B_val)
+
+def loop():
+	while True:
+		for col in colors:
+			setColor(col)
+			time.sleep(1)
+
+def destroy():
+	p_R.stop()
+	p_G.stop()
+	p_B.stop()
+	off()
+	GPIO.cleanup()
+
+if __name__ == "__main__":
+	try:
+		setup(R, G, B)
+		loop()
+	except KeyboardInterrupt:
+		destroy()
