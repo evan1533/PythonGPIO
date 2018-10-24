65e5be8353ba0489bc10525b404a9fd52059ec76
evan1533 <33203039+evan1533@users.noreply.github.com>
2018-10-24 19:11:33 -0400

    Update RGBTest.py

--- RGBTest.py
+++ RGBTest.py
@@ -3,9 +3,9 @@ import RPi.GPIO as GPIO
 import time
 
 colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF]
-R = 3                                                                                                                                    
-G = 4
-B = 5
+R = 22                                                                                                                                    
+G = 23
+B = 24
 
 def setup(Rpin, Gpin, Bpin):
 	global pins
